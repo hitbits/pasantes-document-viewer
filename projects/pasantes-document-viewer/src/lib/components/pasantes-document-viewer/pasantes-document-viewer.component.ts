b3cd7def0d56587a1cec9841fae175691fa03dde
Javier Sánchez Ochando <jsanchez@deskblue.com>
2019-06-24 18:01:36 +0200

    [DEV] Add 'source' as input property to load pdf documents

--- projects/pasantes-document-viewer/src/lib/components/pasantes-document-viewer/pasantes-document-viewer.component.ts
+++ projects/pasantes-document-viewer/src/lib/components/pasantes-document-viewer/pasantes-document-viewer.component.ts
@@ -2,6 +2,8 @@
 import { AfterViewInit,
          Component,
          ElementRef,
+         Input,
+         OnDestroy,
          ViewChild      } from '@angular/core';
 
 // pdfjs module dependencies
@@ -14,17 +16,54 @@ import { PDFViewer      } from 'pdfjs-dist/web/pdf_viewer.js';
   templateUrl: './pasantes-document-viewer.component.html',
   styleUrls: [ './pasantes-document-viewer.component.scss' ]
 })
-export class PasantesDocumentViewerComponent implements AfterViewInit {
+export class PasantesDocumentViewerComponent implements AfterViewInit, OnDestroy {
   @ViewChild('container')
   private container: ElementRef<HTMLElement>;
   private viewer: any;
 
+  private loadingTask: any;
+  private document: any;
 
-  ngAfterViewInit() {
+  @Input()
+  set source(source: string) {
+    // checks that source is not null or empty
+    if (!source) {
+      return;
+    }
+
+    // clear previous loading tasks
+    if (this.loadingTask) {
+      this.loadingTask.destroy();
+      this.loadingTask = null;
+    }
+
+    // load the requested source
+    this.loadingTask = getDocument(source);
+    this.loadingTask.promise.then(document => {
+      this.document = document;
+      this.viewer.setDocument(document);
+    });
+  }
+
+  public ngAfterViewInit() {
     // initialize pdf viewer
     this.viewer = new PDFViewer({
       container: this.container.nativeElement,  // set container element
       textLayerMode: 0                          // disable text layer
     });
+  }
+
+  public ngOnDestroy() {
+    // clear loading task
+    if (this.loadingTask) {
+      this.loadingTask.destroy();
+      this.loadingTask = null;
+    }
+
+    // clear document proxy
+    if (this.document) {
+      this.document.destroy();
+      this.document = null;
+    }
   }
 }
