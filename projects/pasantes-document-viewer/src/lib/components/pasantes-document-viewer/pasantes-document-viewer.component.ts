600be15fb1224eb43681aa6ac1b6b0568376b0fd
Javier Sánchez Ochando <jsanchez@deskblue.com>
2019-06-24 10:02:19 +0200

    [DEV] Initialize pdf viewer

--- projects/pasantes-document-viewer/src/lib/components/pasantes-document-viewer/pasantes-document-viewer.component.ts
+++ projects/pasantes-document-viewer/src/lib/components/pasantes-document-viewer/pasantes-document-viewer.component.ts
@@ -1,5 +1,12 @@
 // angular dependencies
-import { Component } from '@angular/core';
+import { AfterViewInit,
+         Component,
+         ElementRef,
+         ViewChild      } from '@angular/core';
+
+// pdfjs module dependencies
+import { getDocument    } from 'pdfjs-dist/build/pdf.js';
+import { PDFViewer      } from 'pdfjs-dist/web/pdf_viewer.js';
 
 
 @Component({
@@ -7,4 +14,17 @@ import { Component } from '@angular/core';
   templateUrl: './pasantes-document-viewer.component.html',
   styleUrls: [ './pasantes-document-viewer.component.scss' ]
 })
-export class PasantesDocumentViewerComponent {}
+export class PasantesDocumentViewerComponent implements AfterViewInit {
+  @ViewChild('container')
+  private container: ElementRef<HTMLElement>;
+  private viewer: any;
+
+
+  ngAfterViewInit() {
+    // initialize pdf viewer
+    this.viewer = new PDFViewer({
+      container: this.container.nativeElement,  // set container element
+      textLayerMode: 0                          // disable text layer
+    });
+  }
+}
